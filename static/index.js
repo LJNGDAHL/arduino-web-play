5520344f03c0d214d298197c566d71fa9835cfb8
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-09-13 16:35:12 +0200

    Tranfer simple Uint8array works, but still not ino file.

--- static/index.js
+++ static/index.js
@@ -9,12 +9,8 @@
   const connectButton = document.querySelector('.connect-js');
   connectButton.addEventListener('click', () => connect());
 
-  const disconnectButton = document.querySelector('.disconnect-js');
-  disconnectButton.addEventListener('click', () => disconnect(device));
-
   const form = document.querySelector('.form-js');
-  form.addEventListener('submit', upload);
-
+  form.addEventListener('submit', () => upload(event));
 
 
   /**
@@ -26,67 +22,31 @@
     navigator.usb.requestDevice({
       filters: [{ vendorId: 0x2341 }] // 0x2341 = Arduino
     })
-      .then(selectedDevice => device = selectedDevice )
-      .then(() => device.open()) // Begin a session
-      .then(() => device.selectConfiguration(1))
-      .then(() => device.claimInterface(1)) // Request exclusive control
-
-
-      /* This is from Chromes USB tests.
-       * TODO: If you manage to get this to work,
-       * exchange code further down.
-       *
-       * https://chromium.googlesource.com/chromium/src.git/+/lkgr/third_party/WebKit/LayoutTests/usb/usbDevice.html
-       */
-      .then(() => device.controlTransferOut({
-        requestType: 'class',
-        recipient: 'interface',
-        request: 0x42,
-        value: 0x1234,
-        index: 0x0001
-      }, new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8])))
-      .then(result => {
-        console.log(result instanceof USBOutTransferResult);
-        console.log(result.status, 'ok');
-        console.log(result.bytesWritten, 8);
-        if (result.status === 'stall') {
-          editorStatus.innerText = 'Upload stalled.';
-        } else if (result.status === 'ok') {
-          editorStatus.innerText = 'Upload accepted.';
-        }
-        return device.close();
-      })
-      /* This is where Chrome's test ends. */
-
-
+      .then(selectedDevice => device = selectedDevice)
+      .then(() => console.log(device))
       .then(() => {
-        console.log(device); // Used during testing
         headline.innerHTML = `You have currently connected an <span class="u-italic">${device.manufacturerName}</span>.`;
-      });
+        connectButton.removeEventListener('click', () => connect());
+        connectButton.innerText = 'Connected';
+        connectButton.classList.add('Button--connected');
+        connectButton.setAttribute('disabled', '');
+      })
+      .catch(err => console.error(err));
   };
 
   /**
-   * End the session with current device by running device.close()
-   * @param {object} device The current device that's connected
+   * Sends code to compiler and, if successful, converts the returned data to an
+   * arrayBuffer, which is later converted to an Uint8Array. The Uint8Array is
+   * transferred to the USB device.
+   *
+   * @param {object} event
    */
-  const disconnect = device => {
-    device.controlTransferOut({
-      'requestType': 'class',
-      'recipient': 'interface',
-      'request': 0x22,
-      'value': 0x00,
-      'index': 0x0001})
-      .then(() => device.close())
-      .then(() => headline.innerHTML = 'No device connected.');
-  };
-
-  function upload(event) {
+  const upload = (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
+    editorStatus.innerText = 'Sends code to compiler, hang in there...';
 
-    const body = {
-      editor: formData.get('editor')
-    };
+    const formData = new FormData(event.target);
+    const body = { editor: formData.get('editor') };
 
     fetch('/compile', {
       method: 'POST',
@@ -97,39 +57,45 @@
     })
       .then(body => {
         if (!body.ok) {
-          // TODO: Elaborate of course.
-          editorStatus.innerText = 'Oh no! Something is not good.';
+          editorStatus.innerText = 'Something is wrong with your code.';
           return;
         } else {
           editorStatus.innerText = 'Looks good! Let\'s start uploading...';
         }
-        // arrayBuffer is needed when transferring data
+
+        // Return the data as an arrayBuffer, since it's needed when converting to Uint8Array
         return body.arrayBuffer();
       })
       .then(buffer => {
-        /* More info about setup: https://wicg.github.io/webusb/#transfers */
-        return device.controlTransferOut({
-          'requestType': 'class', // Can be 'standard', 'class' or 'vendor'
-          'recipient': 'interface', // 'device', 'interface', 'endpoint' or 'other'
-          'request': 0x22, // Still unclear
-          'value': 0x01, // Still unclear
-          'index': 0x0001 // Seems to be correlate with interface #1
-        })
+        // Data is what we want to transfer to Arduino.
+        const data = new Uint8Array(buffer);
 
-          /* 2 = endpointNumber #2, correlates to interface #2
-           * Trying Uint8Array, since it is used in Chrome's test and
-           * this library that is connecting WebUSB and Arduino:
-           * https://github.com/webusb/arduino/blob/gh-pages/library/WebUSB/WebUSB.h
-           */
-          .then(() => device.transferOut(2, new Uint8Array(buffer)));
+        device.open() // Begin a session
+
+          .then(() => device.selectConfiguration(1))
+          .then(() => device.claimInterface(2))
+          .then(() => device.selectAlternateInterface(2, 0))
+          .then(() => device.controlTransferOut({
+            requestType: 'class',
+            recipient: 'interface',
+            request: 0x22,
+            value: 0x01,
+            index: 0x02
+          }))
+
+          // Sending an Uint8Array works. It's probably something wrong with converting from ino to Uint8Array.
+          .then(() => device.transferOut(4, new Uint8Array([1,2,4])))
+          .then(result => {
+            console.log(`Transfer results: ${result instanceof USBOutTransferResult}, results status: ${result.status}, bytes written: ${result.bytesWritten}`);
+
+            if (result.status === 'ok') {
+              editorStatus.innerText = 'Code uploaded.';
+            }
+
+          })
+          .then(() => device.close());
       })
-      // Only for testing purposes
-      .then(() => console.log(device.controlTransferIn({
-        'requestType': 'class',
-        'recipient': 'interface',
-        'request': 0x22,
-        'value': 0x01,
-        'index': 0x0001}, 64))).catch(err => console.error(err));
-  }
+      .catch(err => console.error(err));
+  };
 }());
 
